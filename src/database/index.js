e74640b7d898f42e7da0243e7a8d4c0c5495a9b1
João Vitor Martins Melo <https://joaoWe@bitbucket.org>
2022-04-14 22:51:52 -0300

    fix: heroku deploy

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
   host: process.env.DB_HOST,
-  port: 5432,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
@@ -17,6 +16,6 @@ client.connect();
 console.log('Connection with DB established');
 
 exports.query = async (query, values) => {
-  const { rows } = await client.query(query, values); // result
-  return rows; // result.rows;
+  const { rows } = await client.query(query, values);
+  return rows;
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// const { response } = require('express');
 const express = require('express');
 require('express-async-errors');
 
@@ -7,18 +6,13 @@ const routes = require('./routes');
 const app = express();
 const cors = require('./app/middlewares/cors');
 const errorHandler = require('./app/middlewares/errorHandler');
-/* // CRIANDO MIDDLEWARES
-app.use((request, response, next) => {
 
-});
- */
 app.use(express.json());
 
-/* cors */
 app.use(cors);
 
 app.use(routes);
 
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(process.env.PORT || 3000, () => console.log('🔥 Server started at http://localhost:3000'));
