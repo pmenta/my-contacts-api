c51a966a0f8e994b2c47903dd6596d814c93bd1a
João Vitor Martins Melo <https://joaoWe@bitbucket.org>
2022-04-14 22:48:54 -0300

    Revert "fix: port to env"
    This reverts commit cc7c55ed3a3ddc9d3456b0eaa23e622b00c621cf.

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
@@ -16,6 +17,6 @@ client.connect();
 console.log('Connection with DB established');
 
 exports.query = async (query, values) => {
-  const { rows } = await client.query(query, values);
-  return rows;
+  const { rows } = await client.query(query, values); // result
+  return rows; // result.rows;
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// const { response } = require('express');
 const express = require('express');
 require('express-async-errors');
 
@@ -6,13 +7,18 @@ const routes = require('./routes');
 const app = express();
 const cors = require('./app/middlewares/cors');
 const errorHandler = require('./app/middlewares/errorHandler');
+/* // CRIANDO MIDDLEWARES
+app.use((request, response, next) => {
 
+});
+ */
 app.use(express.json());
 
+/* cors */
 app.use(cors);
 
 app.use(routes);
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
